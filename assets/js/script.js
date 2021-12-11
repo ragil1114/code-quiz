7f689132e65e9858c6d83d57740d4e660f767851
Ragil14 <rahil.gilani@gmail.com>
2021-12-11 16:53:26 -0600

    added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 var startBtn = document.getElementById("start");
 var timerEl = document.getElementById("timer");
-var contentDiv = document.getElementById("content")
+var contentDiv = document.getElementById("content");
+var qDiv = document.getElementById("questions");
 
 var time = 100;
 var timerInterval;
 
 var gameIndex = -1;
+
 
 function startQuiz() {
     timerEl. textContent = time;
@@ -18,47 +20,55 @@ function startQuiz() {
     }, 1000);
 
     gameIndex++;
-
-    var question = questions[gameIndex];
-
-    var questionDiv = document.createElement("div");
-    var titleP = document.createElement("p");
-    titleP.textContent = question.title;
-
-    for(var i = 0; i < question.choices.length; i++) {
-        var btnEl = document.createElement("button");
-        var choice = question.choices[i];
-        btnEl.textContent = question.choices[1];
-        btnEl.onclick = checkAnswer
-        btnEl.setAttribute("data-answer", question.answer);
-        questionDiv.append(btnEl);
-    }
-    contentDiv.append(questionDiv);
+    showQuestion();
 }
+
 
 function endQuiz() {
-    clearInterval(timerEl);
+    clearInterval(timerInterval);
 }
+
 
 function showQuestion() {
     var question = questions[gameIndex];
     qDiv.innerHTML = '';
     var questionDiv = document.createElement("div");
     var titleP = document.createElement("p");
-    titleP.textContent = qu
+    titleP.textContent = question.title;
+    questionDiv.append(titleP);
+
+    for (var i = 0; i < question.choices.length; i++) {
+        var btnEl = document.createElement("button");
+        var choice = question.choices[i];
+        btnEl.textContent = choice;
+        btnEl.onclick = checkAnswer;
+        btnEl.setAttribute("data-answer", question.answer);
+        questionDiv.append(btnEl);
+    }
+    qDiv.append(questionDiv);
 }
+
 
 function checkAnswer(choice, answer) {
     var answer = event.target.getAttribute("data-answer");
     var choice = event.target.textContent;
     if (choice === answer) {
-        console.log("You got it right!")
-    
-    } else {
+        console.log("CORRECT!")
+    } 
+    else {
         time -= 10;
+        console.log("WRONG")
     }
     gameIndex++;
-    showQuestion
+    showQuestion();
 }
 
+
 startBtn.onclick = startQuiz;
+
+
+// var question = questions[gameIndex];
+
+// var questionDiv = document.createElement("div");
+// var titleP = document.createElement("p");
+// titleP.textContent = question.title;
